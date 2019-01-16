c5bef2546e6037de097890ad4d2dde10dbc7d568
Christopher <christopher.carnivale@gmail.com>
2019-01-16 11:23:05 -0500

    First commit

---
+++ objdrills.js
@@ -0,0 +1,44 @@
+'use strict';
+function createMyObject (){
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function(){
+      return 'hello';
+    },
+  };
+}
+
+function updateObject(obj){
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: `${this.firstName} ${this.lastName}`,
+  };
+  return person;
+}
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar; 
+  return obj;
+}
+
