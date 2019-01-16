084b23315832fc0399cfe474abf08b0945250fbd
matth3wn <matth3wn@yahoo.com>
2019-01-16 14:26:58 -0800

    Update objdrills.js
    added bonus exercises

--- objdrills.js
+++ objdrills.js
@@ -228,24 +228,149 @@
 // };
 // console.log (food.meals[3]);
 
-const obj2 = {
-  name: 'Hunter',
-  job: 'Journalist',
+// const obj2 = {
+//   name: 'Hunter',
+//   job: 'Journalist',
+// };
+
+// const obj3 = {
+//   name: 'Hunter2',
+//   job: 'Journalist',
+//   boss: obj2.name,
+// };
+
+// const obj4 = {
+//   name: 'Hunter3',
+//   job: 'Journalist',
+//   boss: obj2.name,
+// };
+
+// const arr = [obj2, obj3, obj4];
+
+// arr.map(obj => obj.hasOwnProperty('boss') ? console.log(`${obj.job} ${obj.name} reports to ${obj.boss}`) : console.log(`${obj.job} ${obj.name} reports to no one.`)); 
+
+// // cracking the code
+
+// const cipher = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+//   d: 4
+// };
+
+// function decodeWord(word) {
+//   switch (word[0]) {
+//   case 'a': 
+//     return word[cipher.a];
+//   case 'b': 
+//     return word[cipher.b];
+//   case 'c': 
+//     return word[cipher.c];
+//   case 'd': 
+//     return word[cipher.d];
+  
+//   default:
+//     return ' ';
+//   }
+// }
+
+// function decode(sen){
+//   return sen.split(' ').map(i=>decodeWord(i)).join('');
+// }
+// //[aba,bas]
+// // for(let i = 0 ; i < arr.length ; i++){
+// //   decode(arr[i]);
+// // }
+// //console.log(decode('aba bas'));
+
+// // Factory Functions with LOTR
+
+// function createCharacter(name, race, origin, attack, defense){
+//   return{
+//     name, race,origin, attack, defense,
+//     describe: function(){
+//       console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
+//     },
+//     evaluateFight: function(char){
+//       return `Your opponent takes ${this.attack - char.defense} damage and you receive ${this.defense >= char.attack? 0: this.defense -char.attack} damage`;
+//     }
+//   };
+// }
+
+// let Gandalf  = createCharacter('Gandalf the White','Wizard','Middle Earth',10,6);
+// let Bilbo   = createCharacter('Bilbo Baggins','Hobbit','The Shire',2,1);
+// let Frodo  = createCharacter('Frodo Baggins','Hobbit','The Shire',3,2);
+// let Aragorn   = createCharacter('Aragorn son of Arathorn','Man','Dunnedain',6,8);
+// let Legolas  = createCharacter('Legolas','Elf','Woodland Realm',8,5);
+
+// let charArr = [Gandalf, Bilbo, Aragorn, Frodo,Legolas];
+
+// charArr.push(createCharacter('Arwen Undomiel','Half Elf', 'Rivendell',10,10));
+// charArr.find(i => i.name === 'Aragorn son of Arathorn').describe();
+
+// charArr.filter(i=> i.race === 'Hobbit');
+
+// charArr.filter(i=> i.attack > 5);
+// Gandalf.evaluateFight(Frodo);
+
+//How would you change the factory function and other methods?
+
+// You would have to add a weapon prop then change the describe to include the weapon.
+
+// 8. BONUS: A Database Search
+
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function findOne(arr, query){
+  let result = arr.find(obj => {
+    let keys = Object.keys(query); //[id, name, squad]
+    for(let i = 0; i < keys.length; i++){
+      if(obj[keys[i]] !== query[keys[i]]){
+        return false;
+      }
+    }
+    return true;
+  });
+  return result;
+}
+
+//console.log(findOne(HEROES, { squad: 'Justice Ha' }));
+
+const Database = {
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' },
+    ]
+  },
+
+  findOne: function(query){
+    let result = this.store.heroes.find(obj => {
+      let keys = Object.keys(query); //[id, name, squad]
+      for(let i = 0; i < keys.length; i++){
+        if(obj[keys[i]] !== query[keys[i]]){
+          return false;
+        }
+      }
+      return true;
+    });
+    return result;
+  }
 };
 
-const obj3 = {
-  name: 'Hunter2',
-  job: 'Journalist',
-  boss: obj2.name,
-};
+console.log(Database.findOne({ id: 3, squad: 'Avengers' }));
 
-const obj4 = {
-  name: 'Hunter3',
-  job: 'Journalist',
-  boss: obj2.name,
-};
-
-const arr = [obj2, obj3, obj4];
-
-arr.map(obj => obj.hasOwnProperty('boss') ? console.log(`${this.job} ${this.name} reports to ${this.boss}`) : console.log(`${this.job} ${this.name} reports to no one.`)); 
 
